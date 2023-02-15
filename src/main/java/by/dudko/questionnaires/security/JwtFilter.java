f97735c9735f2fd63af6c6a1fddcf680aa20fb7b
Dudko Mikhail <dudkomikhail01@gmail.com>
2023-02-15 10:48:43 +0300

    implemented jwt logic

---
+++ src/main/java/by/dudko/questionnaires/security/JwtFilter.java
@@ -0,0 +1,57 @@
+package by.dudko.questionnaires.security;
+
+import by.dudko.questionnaires.util.JwtUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+    private final JwtUtils jwtUtils;
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
+        String jwtToken = extractToken(request);
+        if (jwtToken == null) {
+            chain.doFilter(request, response);
+            return;
+        }
+        if (jwtUtils.isTokenValid(jwtToken) && SecurityContextHolder.getContext().getAuthentication() == null) {
+            Authentication authentication = buildAuthentication(jwtToken, request);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        chain.doFilter(request, response);
+    }
+
+    private String extractToken(HttpServletRequest request) {
+        String authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
+            return null;
+        }
+        return authorizationHeader.substring("Bearer ".length());
+    }
+
+    private Authentication buildAuthentication(String token, HttpServletRequest request) {
+        String email = jwtUtils.extractUserName(token);
+        UserDetails userDetails = userDetailsService.loadUserByUsername(email);
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                userDetails, null, userDetails.getAuthorities()
+        );
+        authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        return authenticationToken;
+    }
+}
---
+++ src/main/java/by/dudko/questionnaires/util/JwtUtils.java
@@ -0,0 +1,90 @@
+package by.dudko.questionnaires.util;
+
+import by.dudko.questionnaires.dto.user.UserDetailsImpl;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.JwtParser;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Slf4j
+@Component
+public class JwtUtils {
+    private final int liveTime;
+    private final Key signingKey;
+    private final String issuer;
+
+    public JwtUtils(@Value("${app.jwt.live-time}") int liveTime,
+                    @Value("${app.jwt.secret}") String secretKey,
+                    @Value("${app.jwt.issuer}") String issuer) {
+        this.liveTime = liveTime;
+        this.signingKey = buildSigningKey(secretKey);
+        this.issuer = issuer;
+    }
+
+    public String generateToken(UserDetailsImpl userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+    public String generateToken(Map<String, Object> claims, UserDetailsImpl userDetails) {
+        Date now = new Date();
+        Date expirationDate = new Date(now.getTime() + liveTime);
+        return Jwts.builder()
+                .addClaims(claims)
+                .claim("id", userDetails.getId())
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(now)
+                .setExpiration(expirationDate)
+                .setIssuer(issuer)
+                .signWith(signingKey, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public boolean isTokenValid(String token) {
+        JwtParser jwtParser = Jwts.parserBuilder()
+                .setSigningKey(signingKey)
+                .requireIssuer(issuer)
+                .build();
+        try {
+            jwtParser.parseClaimsJws(token);
+            return true;
+        } catch (JwtException e) {
+            log.warn("Invalid token: " + token, e);
+            return false;
+        }
+    }
+
+    public Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(signingKey)
+                .requireIssuer(issuer)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> resolver) {
+        return resolver.apply(extractAllClaims(token));
+    }
+
+    private Key buildSigningKey(String secretKey) {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+
+    public String extractUserName(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+}
