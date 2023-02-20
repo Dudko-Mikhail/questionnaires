78ec3dba2db0b58f012670638804a46365edfe37
Dudko Mikhail <dudkomikhail01@gmail.com>
2023-02-20 11:29:48 +0300

    added custom exception

---
+++ src/main/java/exception/UserNotFoundException.java
@@ -0,0 +1,22 @@
+package exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException() {
+    }
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+    public UserNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public UserNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    public UserNotFoundException(long userId) {
+        super("User with id: " + userId + " not found");
+    }
+}
